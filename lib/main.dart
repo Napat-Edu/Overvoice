f95192e563d48bc46b90c2f31c709b400a7ab248
Niwatchai Wangtrakuldee <niwatchai.wang@mail.kmutt.ac.th>
2022-10-30 11:32:24 +0700

    Add route

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,39 @@ import 'package:provider/provider.dart';
 import 'controller/login_controller.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    title: 'TestNaja',
+    home: FirstRoute(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Login Page'),
+          // Within the `FirstRoute` widget
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -29,3 +57,23 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+/*
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          // Within the SecondRoute widget
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+  */
